78cf48a5ff40f0c35b17daf6d63cf2ece583d45a
leonoha <159581573+leonoha@users.noreply.github.com>
2024-02-09 23:08:10 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ fake_history = [
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://p10-client.onrender.com");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
